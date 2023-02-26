97f3bf2f237b01eab430eab4be6be621962a7bbe
Ronan Boiteau <ronan@boiteau.ovh>
2023-02-26 03:15:02 +0100

    Fix invalid paths

--- safe_template_engine/engine.py
+++ safe_template_engine/engine.py
@@ -5,8 +5,8 @@ class SafeTemplateEngine(Engine):
 
     def __init__(self, **kwargs):
         self.default_builtins = [
-            'safe_template_rendering.trusted_filters',
-            'safe_template_rendering.trusted_tags',
+            'safe_template_engine.trusted_filters',
+            'safe_template_engine.trusted_tags',
         ]
 
         super().__init__(self, **kwargs)
