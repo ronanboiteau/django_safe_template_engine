a27c5943ddbc697260e9bde32f6155b5af9c7514
Ronan Boiteau <ronan@boiteau.eu>
2023-03-03 13:27:27 +0100

    Update tests

--- tests/filters/test_untrusted_filters.py
+++ tests/filters/test_untrusted_filters.py
@@ -6,19 +6,22 @@ from django.template.exceptions import TemplateSyntaxError
 from safe_template_engine.engine import SafeTemplateEngine
 
 
-class TestUntrustedTags:
+class TestUntrustedFilters:
     engine = SafeTemplateEngine()
 
     def _render(self, template_code, context={}):
         template = Template(template_code, engine=self.engine)
         return template.render(Context(context, autoescape=False))
 
-    def _msg_regex(self, tag_name):
-        return rf"^Invalid block tag on line .+: '{tag_name}'"
+    def _msg_regex(self, filter_name):
+        return rf"^Invalid filter: '{filter_name}'"
 
     def test_do_not_trust_load(self):
         with pytest.raises(
             TemplateSyntaxError,
-            match=self._msg_regex('load'),
+            match=self._msg_regex('pprint'),
         ):
-            self._render('{% load i18n %}')
+            self._render(
+                '{{ test_list|pprint }}',
+                context={'test_list': []}
+            )
